0cbc0939e2cf10761587ec4bffbb72732e88fca8
Dhaval <dhavallaiya@gmail.com>
2020-05-03 18:53:11 +0530

    Remove prafanity and Add event ack

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,10 +16,24 @@ app.use(express.static(publicDirectoryPath));
 // sender (emit) => receiver (on)
 io.on('connection', (socket) => {
   socket.emit('message', 'Welcome!');
+
   socket.broadcast.emit('message', 'A new user has joined!');
-  socket.on('sendMessage', (message) => {
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+
     io.emit('message', message);
+    callback();
   });
+
+  socket.on('sendLocation', ({ latitude, longitude }, callback) => {
+    socket.broadcast.emit('message', `https://www.google.com/maps?q=${latitude},${longitude}`);
+    callback();
+  });
+
   socket.on('disconnect', () => {
     io.emit('message', 'A user has left!');
   })
--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -11,5 +11,28 @@ socket.on('message', (message) => {
 document.querySelector('#message-form').addEventListener('submit', (e) => {
   e.preventDefault();
   const message = e.target.elements.message.value;
-  socket.emit('sendMessage', message);
+  socket.emit('sendMessage', message, (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    console.log('Message delieved!');
+  });
+});
+
+document.querySelector('#send-location').addEventListener('click', (e) => {
+  e.preventDefault();
+  if (!navigator.geolocation) {
+    alert('Geolocation is not supported by you browser');
+  }
+  navigator.geolocation.getCurrentPosition(
+    (pos) => {
+      const { latitude, longitude } = pos.coords;
+      socket.emit('sendLocation', {
+        latitude, longitude
+      }, () => {
+        console.log('Location shared!');
+      });
+    }, (err) => {
+      alert(err.message);
+    });
 })
