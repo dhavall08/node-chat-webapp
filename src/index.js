0b71722458f1de162c7c5fbb3efc44d3fae2eda6
Dhaval <dhavallaiya@gmail.com>
2020-05-03 16:41:55 +0530

    Broadcast event

--- src/index.js
+++ src/index.js
@@ -15,8 +15,12 @@ app.use(express.static(publicDirectoryPath));
 // sender (emit) => receiver (on)
 io.on('connection', (socket) => {
   socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new user has joined!');
   socket.on('sendMessage', (message) => {
     io.emit('message', message);
+  });
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left!');
   })
 });
 
